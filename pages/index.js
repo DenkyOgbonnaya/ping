1ef8cd57dacce1a8c4a6ae133a187c298ea01d63
DenkyOgbonnaya <denkyogb@gmail.com>
2019-10-29 15:54:25 -0700

    Make deployment changes

--- pages/index.js
+++ pages/index.js
@@ -4,18 +4,10 @@ import {AuthContext} from '../context/authContext';
 import Chatup from '../components/chat/chatup';
 import Router from 'next/router';
 import Spinner from '../components/includes/spinner';
-import { socketInit } from '../actions/authActions';
-import socket from '../actions/socket';
-
 
 const App = () => {
     const{authData, dispatchAuth} = useContext(AuthContext);
 
-    /*useEffect( () => {
-        ( () => {
-            socketInit(socket, dispatchAuth);
-        } )();
-    }, [])*/
     useEffect( () => {
        const isUserAuth = () => {
            if(!authData.isAuthenticated)
